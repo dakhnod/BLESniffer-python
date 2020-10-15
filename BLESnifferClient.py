9f4e185b0dcc3f02b452c07d79f176186c7dabad
Daniel Dakhno <dakhnod@gmail.com>
2020-10-15 02:11:49 +0200

    parser now accepts filepath as input file
    useful if adb log pre-recorded

--- BLESnifferClient.py
+++ BLESnifferClient.py
@@ -3,16 +3,24 @@ import os
 
 
 class BLESnifferClient:
-    def __init__(self, package_name):
-        self.adb_process = subprocess.Popen(
-            ['adb', 'logcat', '-s', '-v', 'raw', 'BLESniffer_' + package_name],
-            stdout=subprocess.PIPE,
-        )
+    def __init__(self, package_name=None, input_file=None):
+        if input_file is not None:
+            self.input_file = open(input_file, 'r')
+        elif package_name is not None:
+            self.adb_process = subprocess.Popen(
+                ['adb', 'logcat', '-s', '-v', 'raw', 'BLESniffer_' + package_name],
+                stdout=subprocess.PIPE,
+            )
+        else:
+            raise Exception("neither package name nor file given")
 
     def get_evt(self):
         while True:
-            line = self.adb_process.stdout.readline()
-            line = line.decode('utf-8')
+            if self.input_file is None:
+                line = self.adb_process.stdout.readline()
+                line = line.decode('utf-8')
+            else:
+                line = self.input_file.readline()
             parts = line.split(' ')
             if parts.__len__() < 4:
                 continue
@@ -27,4 +35,3 @@ class BLESnifferClient:
                 'payload': payload,
                 'characteristic': parts[2]
             }
-
